d82e7d140240e18e9d51280bbddb03fba9a2e932
pewpewnor <newpatchnotworking2@tutanota.com>
2023-03-01 18:13:50 +0700

    Refactor and fix navbar components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import Navbar from "./components/navbar/Navbar.js"
+import ThreadView from "pages/home/ThreadView.js";
+import Navbar from "./components/navbar/Navbar.js";
 
 export default function App() {
-  const user = {
-    id: 'ketsu_no_ana117',
-    subscription: 'Milky Way Astronout',
-    profilePicture: 'https://i.pximg.net/img-master/img/2023/02/24/09/23/10/105661350_p0_master1200.jpg'
-  }
+	const user = {
+		id: "ketsu_no_ana117",
+		subscription: "Milky Way Astronaut",
+		profilePicture:
+			"https://i.pximg.net/img-master/img/2023/02/24/09/23/10/105661350_p0_master1200.jpg",
+	};
 
-  return (
-    <Navbar User={user} isLogin={true}/>
-  );
+	return (
+		<div>
+			<Navbar User={user} isLogin={true} />
+			<ThreadView />
+		</div>
+	);
 }
