626595a11015536a90a27dcfc5b4b6da56cdce01
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-03-01 18:13:50 +0700

    Refactor and fix navbar components

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,61 +1,24 @@
-import "./Navbar-style.css"; // Importing styles
-import { Icon } from "@iconify/react";
+import { useContext } from "react";
+import UserContext from "contexts/UserContext";
+import AccountInformation from "./AccountInformation";
+import "./navbar.css";
+import { FaCrown } from "react-icons/fa";
 
-function AccountInformation({ User, isLogin }) {
-	let iconMembership;
-	// ntar harus ditambahin rank lain
-	if (User.subscription === "Milky Way Astronout") {
-		iconMembership = "icon-membership-yellow";
-	}
-
-	if (isLogin) {
-		return (
-			<div className="account-information-container">
-				<div className="user-information-data-container">
-					<div className="user-username-container">
-						<p className="user-ast">ast/</p>
-						<p className="user-username">{User.id}</p>
-					</div>
-
-					<div className="user-subscription-container">
-						<p className="user-subscription">{User.subscription}</p>
-						<Icon
-							icon="ph:crown-simple-fill"
-							className={iconMembership}
-						/>
-					</div>
-				</div>
-
-				<div className="border"></div>
-
-				{/* Help fix*/}
-				<div className="user-profilePicture">
-					{/* <img src={link} alt='photo'></img> */}
-				</div>
-			</div>
-		);
-	} else {
-		return <button className="sign-in-button">Sign in</button>;
-	}
-}
-
-export default function Navbar({ User, isLogin }) {
+export default function Navbar() {
 	function handleSearch(event) {
 		if (event.key === "Enter") {
-			// Placeholder
 			alert("User clicked enter");
 		}
 	}
 
 	function handleCreatePost(e) {
-		// Placeholder
 		alert("User wants to create a new thread");
 	}
 
 	return (
 		<div className="navbar-container">
 			<div className="logo-container">
-				<img src={"/assets/hyperspace-logo.png"} alt="logo"></img>
+				<img src="assets/hyperspace-logo.png" alt="logo"></img>
 				<div className="logo-name-container">
 					<h1 id="logo-first">Hyper</h1>
 					<h1 id="logo-second">Space</h1>
@@ -65,15 +28,12 @@ export default function Navbar({ User, isLogin }) {
 			<div className="search-bar-container">
 				<div className="search-bar">
 					<span>
-						<Icon
-							icon="ic:baseline-search"
-							className="search-icon"
-						/>
+						<FaCrown />
 					</span>
 					<input
 						className="search-input"
 						placeholder="Search anything here..."
-						onKeyDown={handleSearch}
+						onClick={handleSearch}
 					></input>
 				</div>
 			</div>
@@ -82,7 +42,7 @@ export default function Navbar({ User, isLogin }) {
 				<p>+</p>
 			</button>
 
-			<AccountInformation isLogin={isLogin} User={User} />
+			<AccountInformation />
 		</div>
 	);
 }
