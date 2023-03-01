3b696eff52beeb0ea05a387c650952988338ee7a
Hans Arthur <hans.cupiterson@binus.ac.id>
2023-03-01 21:03:00 +0700

    Fixing some components and added profile picture to Navbar

--- src/components/navbar/AccountInformation.js
+++ src/components/navbar/AccountInformation.js
@@ -12,6 +12,7 @@ export default function AccountInformation() {
 
 	const user = findUser(userInfo.userID);
 
+
 	return (
 		<div className="account-information-container">
 			<div className="user-information-data-container">
@@ -21,16 +22,15 @@ export default function AccountInformation() {
 
 				<div className="user-subscription-container">
 					<p className="user-subscription">{user.subscription}</p>
-					<FaCrown />
+					<FaCrown className="icon-membership-yellow" />
 				</div>
 			</div>
 
 			<div className="border"></div>
 
-			<div className="user-profilePicture">
-				{/* <img src={user.profilePicture} alt="photo"></img> */}
-				<img src="#" alt="photo"></img>
-			</div>
+			<img src={user.profilePicture} alt={user.name} className="user-profile-picture"></img>
+			
+			{/* <img src="" alt="photo"></img> */}
 		</div>
 	);
 }
