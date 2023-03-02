a55f2c60a121f84938a68fefcd1bfb80e3864798
Hans Arthur <hans.cupiterson@binus.ac.id>
2023-03-02 22:16:29 +0700

    Added CSS for Thread and Modify some logic for Navbar Css

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import Navbar from "./components/navbar/Navbar.js";
 import userData from "data/userdata.js";
 import UserContext from "contexts/UserContext.js";
 
+//import css
+import "./style.css"
+
 export default function App() {
 	return (
-		<div>
+		<div className="body__container">
 			<UserContext.Provider
 				value={{ isLoggedIn: true, userID: userData[0].ID }}
 			>
 				<Navbar />
-				<ThreadView />
+				<ThreadView/>
 			</UserContext.Provider>
 		</div>
 	);
