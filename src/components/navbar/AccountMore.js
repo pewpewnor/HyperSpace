57b088ec62a66a35707ed01f0eaceb14e7993084
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-03-02 19:36:31 +0700

    Refactor and modify navbar innerworkings
    History-Rewrite-Original-Commit: 3d0e3525f9d9f8af653a4d4c6844e50f646e4c61

--- src/components/navbar/AccountMore.js
+++ src/components/navbar/AccountMore.js
@@ -1,11 +1,15 @@
+import { useEffect } from "react";
 import "./navbar.css";
 
-export default function AccountMorePopUp(){
+export default function AccountMorePopUp() {
+    useEffect(() => {
+        alert("account more pop up!");
+    });
+
     return(
         <div className="background">
             <div className="account-more-container">
                 <div className="button-container">
-                    {alert('Hello World!')}
                     <button>My Profile</button>
                     <button>Sign out</button>
                 </div>
