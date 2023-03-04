45558178fc5dc210eb2c6a3370dbd5e9f4138dca
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-03-04 23:45:17 +0700

    Add space and channel functionality and data
    History-Rewrite-Original-Commit: d83aab47b8226285ecb5f52fa6220f1552b3ad85

--- src/pages/home/ThreadView.js
+++
@@ -1,9 +0,0 @@
-import Thread from "components/thread/Thread";
-import threadData from "data/threaddata";
-
-export default function ThreadView() {
-	const threads = threadData.map((thread) => {
-		return <Thread key={thread.ID} {...thread} />;
-	});
-	return <div>{threads}</div>;
-}
