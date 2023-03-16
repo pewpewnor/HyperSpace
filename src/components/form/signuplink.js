47d662aee677812186ef3f24a3d0c363bc148889
Gavriel <gavriel.reynard@binus.ac.id>
2023-03-16 17:51:26 +0700

    finished register page and refactor
    - register done
    - refactored form in components (submit.js, submit.css, signuplink.js, signuplink.css)
    
    
    History-Rewrite-Original-Commit: c1fce543f7aa75d4d023b4baf006a5cacb371427

--- src/components/form/signuplink.js
+++ src/components/form/signuplink.js
@@ -1,10 +1,26 @@
 import "./signuplink.css"
 
-function Signuplink(){
+function Signuplink({className}){
+
+    let text, link, text_design, link_design = "";
+
+    if({className} === "Login") {
+
+        text = "New to hyperSpace?";
+        link = "Sign up here...."
+        text_design = "Login";
+        link_design = "Login";
+    } else {
+        text = "Already have an account?";
+        link = "Login here...."
+        text_design = "Register";
+        link_design = "Register";
+    }
+
     return(
         <div className="text">
-			<span>New to hyperSpace? <br/><a href="">Sign up here....</a></span>
-		</div>
+            <span className={text_design}>{text} <br/><a href="" className={link_design}>{link}</a></span>
+        </div>
     )
 }
 
