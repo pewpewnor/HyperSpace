7f9b9d8081abe6f61203cce3fb7956462f601da3
Hans Arthur <hans.cupiterson@binus.ac.id>
2023-03-22 20:17:11 +0700

    Added Profile Page and make the website responsive
    (still not completed)

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import "./style.css";
 // imports for temporary mockup
 import Home from "./pages/home/Home";
 import SpacePage from "./pages/spacepage/SpacePage";
+import ProfilePage from "./pages/profile/ProfilePage";
 import userData from "data/userdata";
 import spaceData from "data/spacedata";
 
 export default function App() {
 	return (
 		<UserContext.Provider value={{ isLoggedIn: true, user: userData[0] }}>
-			<SpacePage space={spaceData[0]} />
+			<ProfilePage user={userData[0]}/>
+			{/* <SpacePage space={spaceData[0]} /> */}
 			{/* <Home /> */}
 			{/* <Login /> */}
 		</UserContext.Provider>
