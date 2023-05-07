e7b4c0d29d3e17c605f58176caaa4d09c1421697
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-05-07 15:19:34 +0000

    add basic routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-// import SpacePage from "./pages/spacepage/SpacePage";
 import UserContext from "contexts/UserContext";
 import Login from "pages/login/Login";
 import Register from "pages/signup/Register";
+import { Route, Routes } from "react-router-dom";
 import "./style.css";
-
-// imports for temporary mockup
-import spaceData from "data/spacedata";
-import userData from "data/userdata";
+import NotFound from "notfound/NotFound";
 import Home from "./pages/home/Home";
-import ProfilePage from "./pages/profile/ProfilePage";
+import Discover from "./pages/discover/discover.js";
+import spaceData from "data/spacedata";
 import SpacePage from "./pages/spacepage/SpacePage";
-import DiscoverPage from "./pages/discover/discover.js";
+import userData from "data/userdata";
+import Profile from "./pages/profile/ProfilePage";
 
 export default function App() {
 	return (
 		<UserContext.Provider value={{ isLoggedIn: true, user: userData[0] }}>
-			{/* <Register /> */}
-			{/* <ProfilePage user={userData[0]} /> */}
-			{/* <SpacePage space={spaceData[0]} /> */}
-			{/* <Home /> */}
-			<Login />
-			{/* <DiscoverPage /> */}
+			<Routes>
+				<Route path="/" element={<Home />}/>
+				<Route path="/login" element={<Login />}/>
+				<Route path="/register" element={<Register />}/>
+				<Route path="/profile" element={<Profile user={userData[0]} />}/>
+				<Route path="/space" element={<SpacePage space={spaceData[0]} />}/>
+				<Route path="/discover" element={<Discover />}/>
+				<Route path="*" element={<NotFound />}/>
+			</Routes>
 		</UserContext.Provider>
 	);
 }
