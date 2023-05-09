a94be967dee2ee65e88ca0ededd75c200f86df1c
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-05-09 13:54:40 +0700

    remove not used form UI

--- src/components/form/usernameform.js
+++
@@ -1,9 +0,0 @@
-import "./usernameform.css";
-
-export default function usernameform() {
-	return (
-		<div className="username__box">
-			<input type={"text"} placeholder={"username"}></input>
-		</div>
-	);
-}
