7ecf339732f863563e4fef67a2cd31d4cca737c4
pewpewnor <newpatchnotworking2@tutanota.com>
2023-05-10 18:36:18 +0700

    add new channel schema

---
+++ server/Channel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const channelSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	threads: {
+		type: [mongoose.SchemaTypes.ObjectId],
+		default: [],
+		ref: "Thread",
+	},
+});
+
+module.exports = mongoose.Model("Channel", channelSchema);
