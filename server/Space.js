f47595d4cfe792cbd018481022c378ab5293acd6
pewpewnor <newpatchnotworking2@tutanota.com>
2023-05-10 18:35:51 +0700

    add space attribute to schema

--- server/Space.js
+++ server/Space.js
@@ -1,5 +1,23 @@
 const mongoose = require("mongoose");
 
-const spaceSchema = new mongoose.Schema({});
+const spaceSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	spaceDescription: {
+		type: String,
+		default: "",
+	},
+	channelsID: {
+		type: [mongoose.SchemaTypes.ObjectId],
+		default: [],
+		ref: "Channel",
+	},
+	members: {
+		type: Number,
+		default: 1,
+	},
+});
 
 module.exports = mongoose.model("Space", spaceSchema);
