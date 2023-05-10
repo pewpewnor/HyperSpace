bdbc7a39ca8bc881c684766133ccbc8778c9d615
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-05-10 09:22:00 +0700

    add User schema

--- server/server.js
+++ server/server.js
@@ -1,7 +1,41 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+dotenv.config();
+
+const app = express();
 app.use(express.json());
+
+mongoose
+	.connect(process.env.DEV_URI)
+	.then(() => console.log("Database connected!"))
+	.catch((err) => console.log(err));
+
+// Test
+
+const User = require("./User");
+
+async function run() {
+	try {
+		const data = await User.create({
+			name: "ketsu_no_ana117",
+			profilePicture: "assets/profile-pic-1.jpg",
+			subscription: "Lieutenant",
+		});
+		// const data = await User.deleteMany({});
+		console.log(data);
+	} catch (error) {
+		console.error(error);
+	}
+	console.error("Halleluya the run works!");
+}
+
+run();
+
+console.error("Halleluya it works!");
+
+// End test
 
 app.post("/api/test", (req, res) => {
 	console.log("test sucess!");
