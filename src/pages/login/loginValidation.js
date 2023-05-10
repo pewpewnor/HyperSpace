e5c1084dfdb1242f87196bbb568a306525d52f29
steven <stevenplayer25@gmail.com>
2023-05-10 18:34:06 +0700

    css fix v2
    masih ada bug dikit

--- src/pages/login/loginValidation.js
+++ src/pages/login/loginValidation.js
@@ -2,20 +2,19 @@ function loginValidation(data) {
 	const errors = {};
 
 	// const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]{2,6}$/;
-	const password_pattern = /^(?=.*\d)(?=.*[a-z])[a-zA-Z0-9]{&.}$/;
+	const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]$/;
 
-	if (data.email === "") {
+	if (data.email.length < 1) {
 		errors.email = "email is required";
 	}
+
 	// else if(!email_pattern.test(data.email)){
 	//     errors.email = "email is required"
 	// }
 
-	if (data.password === "") {
-		errors.password = "passwword is required";
-	} else if (!password_pattern.test(data.password)) {
+	if (!password_pattern.test(data.password) && data.password.length < 8) {
 		errors.password =
-			"password should contains upper letter, number, and atleast 8 word";
+			"password is required and should contains upper letter, number, and atleast 8 word";
 	}
 
 	return errors;
