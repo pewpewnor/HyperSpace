3c5448e8813f217b425964f329bf7e95a9338170
Hans Arthur <hans.cupiterson@binus.ac.id>
2023-05-10 16:08:34 +0700

    added inputfield component

--- src/components/form/inputfield.js
+++ src/components/form/inputfield.js
@@ -1,10 +1,17 @@
 import "./inputfield.css";
+import { useState } from "react";
 
 function InputField(props) {
+	const [text, setText] = useState("");
+
+	function handleChange(e) {
+		setText(e.target.value);
+	}
+
 	return (
 		<div className="inputField__container">
 			<div className="inputField__label">{props.label}</div>
-			<textarea className="inputField__textArea"></textarea>
+			<textarea className={props.type} onChange={handleChange}></textarea>
 		</div>
 	);
 }
--- src/pages/createthread/createthread.js
+++ src/pages/createthread/createthread.js
@@ -9,7 +9,10 @@ export default function CreateThread() {
 			<Navbar />
 			<div className="create__thread__container">
 				<div className="create__thread__title">Create New Thread</div>
-				<InputField label="Thread Title" />
+				<div className="create__thread__inputField__container">
+					<InputField label="Thread Title" type="inputField__title" />
+					<InputField label="Body" type="inputField__body" />
+				</div>
 			</div>
 		</>
 	);
