27893033681ea45fcf047c835bc52b42091a35eb
pewpewnor <newpatchnotworking2@tutanota.com>
2023-05-10 19:30:29 +0700

    add key attribute

--- server/User.js
+++ server/User.js
@@ -11,6 +11,10 @@ const userSchema = new mongoose.Schema({
 		type: String,
 		required: true,
 	},
+	key: {
+		type: String,
+		default: null,
+	},
 	profilePicture: {
 		type: String,
 		required: true,
