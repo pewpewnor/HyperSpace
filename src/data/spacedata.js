b1d08e4f419d91dc394c896fb1f228bf722abf86
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-05-10 16:15:12 +0700

    change members from string to number

--- src/data/spacedata.js
+++ src/data/spacedata.js
@@ -5,21 +5,21 @@ const spaceData = [
 		channelsID: ["CHA001", "CHA002", "CHA003"],
 		spaceDesctiption:
 			"This is Psychology Community, the best place to search anything related to psychology, Come and join with 6.5M user across the world!",
-		members: "6.5M members",
+		members: 525557,
 	},
 	{
 		ID: "SPA002",
 		name: "Gaming",
 		channelsID: [],
 		spaceDesctiption: "Hello World!",
-		members: "6.5M members",
+		members: 8432475545,
 	},
 	{
 		ID: "SPA003",
 		name: "Cooking",
 		channelsID: [],
 		spaceDesctiption: "Hello Norbert!",
-		members: "6.5M members",
+		members: 65000,
 	},
 
 	{
@@ -27,7 +27,7 @@ const spaceData = [
 		name: "Anime",
 		channelsID: [],
 		spaceDesctiption: "Yor FORGER AAAAAAAAA",
-		members: "6.5M members",
+		members: 7000000000000,
 	},
 ];
 
