21652f9b7d9060aef56d72c05e82bde3890a0f80
pewpewnor <newpatchnotworking2@tutanota.com>
2023-05-10 19:01:10 +0700

    edit validation

--- src/pages/login/loginValidation.js
+++ src/pages/login/loginValidation.js
@@ -1,20 +1,16 @@
 function loginValidation(data) {
 	const errors = {};
 
-	// const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]{2,6}$/;
-	const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]$/;
+	const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]{2,6}$/;
 
 	if (data.email.length < 1) {
 		errors.email = "email is required";
+	} else if (!email_pattern.test(data.email)) {
+		errors.email = "email must be valid";
 	}
 
-	// else if(!email_pattern.test(data.email)){
-	//     errors.email = "email is required"
-	// }
-
-	if (!password_pattern.test(data.password) && data.password.length < 8) {
-		errors.password =
-			"password is required and should contains upper letter, number, and atleast 8 word";
+	if (data.password.length < 1) {
+		errors.password = "password is required";
 	}
 
 	return errors;
