40d894d93f767fbbb7e87d007c62b6afd5d47840
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-05-11 12:24:57 +0700

    add comment mockup data

---
+++ server/mockup/commentData.js
@@ -0,0 +1,18 @@
+const commentData = [
+	{
+		text: "I don't think we should talk to people at all",
+		childCommentsID: [],
+		authorID: "645bcbd39365d8736c77bf5b",
+	},
+	{
+		text: "We are introverted we don't care. Bruh.",
+		childCommentsID: [],
+		authorID: "645bcbd39365d8736c77bf5a",
+	},
+	{
+		text: "What?",
+		authorID: "645bcbd39365d8736c77bf5a",
+	},
+];
+
+module.exports = commentData;
