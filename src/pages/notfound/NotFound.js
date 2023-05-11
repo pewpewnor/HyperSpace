ba2580637424f04817a0e0bc697f10554a7a0552
Hans Arthur <hans.cupiterson@binus.ac.id>
2023-05-11 19:43:40 +0700

    fix some bug

--- src/pages/notfound/NotFound.js
+++ src/pages/notfound/NotFound.js
@@ -20,21 +20,30 @@ export default function NotFound() {
 		document.title = "404 Not Found";
 	}, [currentPage]);
 
-	return <div className="NotFoundPage-container">
+	return (
+		<div className="NotFoundPage-container">
+			<h1 className="NotFoundPage-h1">
+				<span className="NotFoundPage-header-textSpan">4</span>0
+				<span className="NotFoundPage-header-textSpan">4</span> Page
+				Does not Exist
+			</h1>
 
-		<h1 className="NotFoundPage-h1"><span className="NotFoundPage-header-textSpan">4</span>0<span className="NotFoundPage-header-textSpan">4</span> Page Does not Exist</h1>
-
-		<p className="NotFoundPage-p">
-			This page isn't available. Sorry about that.
+			<p className="NotFoundPage-p">
+				This page isn't available. Sorry about that.
 				<br></br>
 				Try searching for something else
 			</p>
 
-		<div className="NotFoundPage-subcontainer">
-			<Link to='/'>
-				<h2 className="NotFoundPage-h2">Head back to home</h2>
-			</Link>
-			<img className="NotFoundPage-logoImg" src={logoImg} alt="Hyperspace Logo" />
+			<div className="NotFoundPage-subcontainer">
+				<Link to="/">
+					<h2 className="NotFoundPage-h2">Head back to home</h2>
+				</Link>
+				<img
+					className="NotFoundPage-logoImg"
+					src={logoImg}
+					alt="Hyperspace Logo"
+				/>
+			</div>
 		</div>
 	);
 }
