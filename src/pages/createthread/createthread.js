fe76b5d62bb8c8fe59af3941c5c6ba424325bda5
Hans Arthur <hans.cupiterson@binus.ac.id>
2023-05-11 19:34:36 +0700

    create-thread page completed
    (still requires routing)

--- src/pages/createthread/createthread.js
+++ src/pages/createthread/createthread.js
@@ -14,31 +14,69 @@ export default function CreateThread() {
 	const [selectedSpaces, setSelectedSpaces] = useState(
 		userData[0].joinedSpacesID[0]
 	);
-	const [selectedChannel, setSelectedChannel] = useState("");
+
+	const [selectedChannel, setSelectedChannel] = useState(
+		findSpace(selectedSpaces).channelsID[0]
+	);
+
+	const [newThread, setNewThread] = useState({
+		space: selectedSpaces,
+		channel: selectedChannel,
+		title: "",
+		body: "",
+		image: "",
+	});
 
 	function handleSpaceChange(event) {
-		const newValue = event.target.value;
-		setSelectedSpaces(newValue);
-		console.log(findSpace(newValue).name);
+		const selected = event.target.value;
+		setSelectedSpaces(selected);
+
+		setNewThread((prev) => ({
+			...prev,
+			space: selected,
+			channel: findSpace(selected).channelsID[0],
+		}));
 	}
 
 	function handleChannelChange(event) {
-		const newValue = event.target.value;
-		setSelectedChannel(newValue);
-		console.log(findChannel(newValue).name);
+		const selected = event.target.value;
+		setSelectedChannel(selected);
+
+		setNewThread((prev) => ({
+			...prev,
+			channel: selected,
+		}));
+	}
+
+	function handleTitleChange(event) {
+		setNewThread((prev) => ({
+			...prev,
+			title: event.target.value,
+		}));
+	}
+
+	function handleBodyChange(event) {
+		setNewThread((prev) => ({
+			...prev,
+			body: event.target.value,
+		}));
+	}
+
+	function handleSubmit(event) {
+		console.log(newThread);
 	}
 
 	return (
 		<div className="create__thread font_size_rule">
 			<Navbar />
 			<div className="create__thread__container font_size_rule">
-				<div className="create__thread__title">
+				<div className="create__thread__title ">
 					<div className="border"></div>
-					<h1>Create New Thread</h1>
+					<h1 className="font_size_rule">Create New Thread</h1>
 				</div>
 				<div className="create__thread__target__container">
 					<div className="create__thread__target__space">
-						<label for="space">Choose Space: </label>
+						<label htmlFor="space">Choose Space: </label>
 						<select
 							name="space"
 							id="spaces"
@@ -48,7 +86,7 @@ export default function CreateThread() {
 						>
 							{/* Index 0 here should be change into userData id */}
 							{userData[0].joinedSpacesID.map((space) => (
-								<option value={space}>
+								<option value={space} key={findSpace(space).ID}>
 									{findSpace(space).name}
 								</option>
 							))}
@@ -58,7 +96,9 @@ export default function CreateThread() {
 					<div className="create__thread__target__channel">
 						{selectedSpaces && (
 							<>
-								<label for="channel">Choose channel: </label>
+								<label htmlFor="channel">
+									Choose channel:{" "}
+								</label>
 								<select
 									name="channel"
 									id="channel"
@@ -68,12 +108,14 @@ export default function CreateThread() {
 								>
 									{findSpace(selectedSpaces).channelsID.map(
 										(channel) => (
-											<option value={channel}>
+											<option
+												value={channel}
+												key={findChannel(channel).ID}
+											>
 												{findChannel(channel).name}
 											</option>
 										)
 									)}
-									;
 								</select>
 							</>
 						)}
@@ -82,10 +124,19 @@ export default function CreateThread() {
 						<InputField
 							label="Thread Title"
 							type="inputField__title"
+							handleChange={handleTitleChange}
 						/>
-						<InputField label="Body" type="inputField__body" />
+						<InputField
+							label="Body"
+							type="inputField__body"
+							handleChange={handleBodyChange}
+						/>
+						<div className="create__thread__inputImage">
+							<h1>Image (optional)</h1>
+							<input type="file" className="input__image"></input>
+						</div>
 					</div>
-					<SubmitBtn />
+					<SubmitBtn name={"Create Thread"} onClick={handleSubmit} />
 				</div>
 			</div>
 		</div>
