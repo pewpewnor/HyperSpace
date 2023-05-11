1620396a2e390f0934061b6accb28d9241796639
Hans Arthur <hans.cupiterson@binus.ac.id>
2023-05-11 10:57:58 +0700

    added submitBtn component

--- src/pages/createthread/createthread.js
+++ src/pages/createthread/createthread.js
@@ -2,6 +2,7 @@ import "./createthread.css";
 import Navbar from "components/navbar/Navbar";
 
 import InputField from "components/form/inputfield";
+import SubmitBtn from "components/ui/submitBtn";
 
 export default function CreateThread() {
 	return (
@@ -9,10 +10,12 @@ export default function CreateThread() {
 			<Navbar />
 			<div className="create__thread__container font_size_rule">
 				<div className="create__thread__title">Create New Thread</div>
+				<div className="create__thread__target__container"></div>
 				<div className="create__thread__inputField__container">
 					<InputField label="Thread Title" type="inputField__title" />
 					<InputField label="Body" type="inputField__body" />
 				</div>
+				<SubmitBtn />
 			</div>
 		</>
 	);
