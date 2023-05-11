c119653a1e7ad398f2b8258e3b83ec446a48c7a8
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-05-12 01:50:21 +0700

    format code

--- server/mockup/userData.js
+++ server/mockup/userData.js
@@ -15,19 +15,13 @@ const userData = [
 		_id: "645bcbd39365d8736c77bf5b",
 		username: "[REDACTED]",
 		password: "[REDACTED]",
-		joinedSpaces: [
-			"645bd00f2b7d749510bc10cb",
-			"645bd00f2b7d749510bc10cc",
-		],
+		joinedSpaces: ["645bd00f2b7d749510bc10cb", "645bd00f2b7d749510bc10cc"],
 	},
 	{
 		_id: "645bcbd39365d8736c77bf5c",
 		username: "[REDACTED]",
 		password: "[REDACTED]",
-		joinedSpaces: [
-			"645bd00f2b7d749510bc10cd",
-			"645bd00f2b7d749510bc10ce",
-		],
+		joinedSpaces: ["645bd00f2b7d749510bc10cd", "645bd00f2b7d749510bc10ce"],
 	},
 ];
 
