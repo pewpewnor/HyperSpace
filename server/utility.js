5778802d138993782a1ad1885027f42cb5457c49
pewpewnor <newpatchnotworking2@tutanota.com>
2023-05-13 00:45:30 +0700

    move functions to utility

--- server/utility.js
+++ server/utility.js
@@ -1,4 +1,12 @@
+const mongoose = require("mongoose");
 const crypto = require("crypto");
+
+const Channel = require("./Channel");
+const ChildComment = require("./ChildComment");
+const Comment = require("./Comment");
+const Space = require("./Space");
+const Thread = require("./Thread");
+const User = require("./User");
 
 function generateKey() {
 	return crypto.randomBytes(48).toString("hex");
@@ -11,6 +19,13 @@ function isBadString(data) {
 	return false;
 }
 
+function isBadStringNullOk(data) {
+	if (data === undefined) return true;
+	if (data === null) return false;
+	if (typeof data !== "string") return true;
+	return false;
+}
+
 function isBadObjectID(id) {
 	if (isBadString(id) || id.length !== 24) {
 		return true;
@@ -18,15 +33,40 @@ function isBadObjectID(id) {
 	return false;
 }
 
-function isBadBoolean(data) {
-	if (data === undefined || typeof data !== "boolean") {
+async function isNotLoggedIn({ key, userID }) {
+	if (isBadString(key) || isBadObjectID(userID)) return true;
+	try {
+		const user = await User.findById(userID);
+		if (!user || user.key !== key) return true;
+	} catch {
 		return true;
 	}
 	return false;
 }
 
+async function deleteAll() {
+	await Channel.deleteMany();
+	await ChildComment.deleteMany();
+	await Comment.deleteMany();
+	await Space.deleteMany();
+	await Thread.deleteMany();
+	await User.deleteMany();
+
+	console.log("Server: All data in database is deleted");
+}
+
+function generateObjectID() {
+	for (let i = 0; i < 4; i++) {
+		console.log(new mongoose.Types.ObjectId());
+	}
+}
+
 module.exports = {
 	generateKey,
 	isBadString,
+	isBadStringNullOk,
 	isBadObjectID,
+	isNotLoggedIn,
+	deleteAll,
+	generateObjectID,
 };
