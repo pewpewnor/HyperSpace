2878dc0050b9b11dd04888f7cab1218759253486
steven <stevenplayer25@gmail.com>
2023-05-12 12:17:26 +0700

    fix css v2.2
    change email to username as login requirement

--- src/pages/login/loginValidation.js
+++ src/pages/login/loginValidation.js
@@ -1,13 +1,14 @@
 function loginValidation(data) {
 	const errors = {};
 
-	const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]{2,6}$/;
+	// const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]{2,6}$/;
 
-	if (data.email.length < 1) {
+	if (data.username.length < 1) {
 		 = "email is required";
-	} else if (!email_pattern.test(data.email)) {
-		 = "email must be valid";
-	}
+	} 
+	// else if (!email_pattern.test(data.email)) {
+	// 	 = "username must be valid";
+	// }
 
 	if (data.password.length < 1) {
 		 = "password is required";
