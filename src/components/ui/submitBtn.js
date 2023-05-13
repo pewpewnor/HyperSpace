c209a1786537b7f4cbc48ee501e5f2839d50eeac
Hans Arthur <hans.cupiterson@binus.ac.id>
2023-05-13 14:32:44 +0700

    Create-channel Release

--- src/components/ui/submitBtn.js
+++ src/components/ui/submitBtn.js
@@ -1,8 +1,13 @@
 import "./submitBtn.css";
 
 export default function SubmitBtn({ name, onClick }) {
+	function handleClick(event) {
+		event.stopPropagation();
+		onClick();
+	}
+
 	return (
-		<button className="submitButton" onClick={onClick}>
+		<button className="submitButton" onClick={handleClick}>
 			{name}
 		</button>
 	);
