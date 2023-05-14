f684fc3516e793dba555bc18a114a4b48b2f4d41
pewpewnor <newpatchnotworking2@tutanota.com>
2023-05-14 21:58:16 +0700

    integrate home page with API

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,22 +1,67 @@
+import Loading from "components/loading/Loading";
 import Navbar from "components/navbar/Navbar";
 import MySpace from "components/space/MySpace";
 import Thread from "components/thread/Thread";
-import threadData from "data/threaddata";
+import UserContext from "contexts/UserContext";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./home.css";
 
-// for mockup data only
-import spaceData from "data/spacedata";
-import channelData from "data/channeldata";
-
 function Home() {
-	const threads = threadData.map((thread) => (
-		<Thread
-			key={thread.ID}
-			space={spaceData[0]}
-			channel={channelData[0]}
-			{...thread}
-		/>
-	));
+	const navigate = useNavigate();
+	const [isLoading, setIsLoading] = useState(true);
+	const [user] = useContext(UserContext);
+
+	const [threads, setThreads] = useState([]);
+
+	useEffect(() => {
+		async function getThreads() {
+			if (user === null) {
+				navigate("/login");
+				return;
+			}
+
+			let threadsData = [];
+
+			try {
+				const res = await fetch(
+					"http://localhost:3001/api/recommendedthreads",
+					{
+						method: "POST",
+						headers: {
+							Accept: "application/json",
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify({ ...user, userID: user._id }),
+					}
+				);
+				if (!res.ok) {
+					navigate("/login");
+					return;
+				}
+				threadsData = await res.json();
+			} catch (error) {}
+
+			setThreads(
+				threadsData.map((data) => (
+					<Thread
+						key={data.id}
+						{...data.thread}
+						user={user}
+						space={data.space}
+						channel={data.channel}
+					/>
+				))
+			);
+			setIsLoading(false);
+		}
+
+		getThreads();
+	}, []);
+
+	if (isLoading) {
+		return <Loading />;
+	}
 
 	return (
 		<div className="all font_size_rule">
