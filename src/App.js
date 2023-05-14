b95e9fd6a7657bd3a935577cd4e36154395ce35c
pewpewnor <newpatchnotworking2@tutanota.com>
2023-05-14 21:57:36 +0700

    set mockup login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import UserContext from "contexts/UserContext";
 import CreateSpace from "pages/createspace/createspace.js";
 import CreateThread from "pages/createthread/createthread.js";
-import NotLoggedIn from "pages/forbidden/NotLoggedIn.js";
+// import NotLoggedIn from "pages/forbidden/NotLoggedIn.js";
 import Login from "pages/login/Login";
 import NotFound from "pages/notfound/NotFound.js";
 import Register from "pages/signup/Register";
@@ -14,7 +14,20 @@ import SpacePage from "./pages/spacepage/SpacePage";
 import "./style.css";
 
 export default function App() {
-	const [user, setUser] = useState(null);
+	// const [user, setUser] = useState(null);
+	const [user, setUser] = useState({
+		_id: "645bcbd39365d8736c77bf5a",
+		username: "ketsu",
+		password: "1234",
+		key: "c381bf796601ad28b2614d60b06471c81fbf7e9253602be498076856ef5a625b62c3afa186bfc1e0fc638ecaff60b1db",
+		joinedSpaces: [
+			"645bd00f2b7d749510bc10cb",
+			"645bd00f2b7d749510bc10cc",
+			"645bd00f2b7d749510bc10cd",
+			"645bd00f2b7d749510bc10ce",
+		],
+		profilePicture: "assets/profile-pic-1.jpg",
+	});
 
 	return (
 		<UserContext.Provider value={[user, setUser]}>
@@ -27,7 +40,7 @@ export default function App() {
 				<Route path="/discover" element={<Discover />} />
 				<Route path="/create-thread" element={<CreateThread />} />
 				<Route path="/create-space" element={<CreateSpace />} />
-				<Route path="/forbidden" element={<NotLoggedIn />} />
+				{/* <Route path="/forbidden" element={<NotLoggedIn />} /> */}
 
 				<Route path="*" element={<NotFound />} />
 			</Routes>
