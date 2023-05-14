70c8ad47800055a23c22bbd26798fb226e0c25a5
pewpewnor <newpatchnotworking2@tutanota.com>
2023-05-14 21:58:26 +0700

    integrate my space with API

--- src/components/space/MySpace.js
+++ src/components/space/MySpace.js
@@ -1,20 +1,66 @@
 import UserContext from "contexts/UserContext";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { findSpace } from "utils/find";
 import Space from "./Space";
 import "./myspace.css";
 
 export default function MySpace() {
-	const { user } = useContext(UserContext);
+	const [user] = useContext(UserContext);
+	const [isLoading, setIsLoading] = useState(true);
 
-	const spaceArray = user.joinedSpacesID.map((spaceID) => {
-		return findSpace(spaceID);
-	});
+	const [joinedSpaces, setJoinedSpaces] = useState([]);
 
-	const spaces = spaceArray.map((space) => {
-		return <Space key={space.ID} {...space} />;
-	});
+	useEffect(() => {
+		async function getSpaces() {
+			let spaces = [];
+
+			try {
+				const resSpaces = await fetch(
+					"http://localhost:3001/api/user/myspace",
+					{
+						method: "POST",
+						headers: {
+							Accept: "application/json",
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify({ ...user, userID: user._id }),
+					}
+				);
+				if (!resSpaces.ok) {
+					return;
+				}
+				spaces = await resSpaces.json();
+			} catch (error) {}
+
+			setJoinedSpaces(
+				spaces.map((space) => {
+					return <Space key={space._id} {...space} />;
+				})
+			);
+			setIsLoading(false);
+		}
+
+		getSpaces();
+	}, []);
+
+	if (isLoading) {
+		return (
+			<div className="myspace">
+				<div className="myspace__top-section">
+					<div className="myspace__top-section__border"></div>
+					<div className="myspace__top-section__title">My Space</div>
+				</div>
+				<div className="myspace__space-view-section"></div>
+				<div className="myspace__button-section">
+					<Link to="/discover">
+						<button className="myspace__button-section__discover-button">
+							Discover more
+						</button>
+					</Link>
+				</div>
+			</div>
+		);
+	}
 
 	return (
 		<div className="myspace">
@@ -22,7 +68,7 @@ export default function MySpace() {
 				<div className="myspace__top-section__border"></div>
 				<div className="myspace__top-section__title">My Space</div>
 			</div>
-			<div className="myspace__space-view-section">{spaces}</div>
+			<div className="myspace__space-view-section">{joinedSpaces}</div>
 			<div className="myspace__button-section">
 				<Link to="/discover">
 					<button className="myspace__button-section__discover-button">
