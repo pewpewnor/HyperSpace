cd45b9fa7392e963d8ddf087cd649faf754a52b7
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-05-14 23:12:37 +0700

    change loading screen

--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -1,14 +1,9 @@
 import "./loading.css";
+
 export default function Loading() {
 	return (
-		<div className="container">
-			<div className="loader-container">
-				<div className="spinner"></div>
-			</div>
-
-			<div className="main-content">
-				<h1 className="loading-h1">Loading...</h1>
-			</div>
+		<div className="loader-container">
+			<div className="spinner"></div>
 		</div>
 	);
 }
