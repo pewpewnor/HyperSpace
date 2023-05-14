b3fcea2fa76081b6be881ba24bf01543315e9ff6
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-05-14 19:00:56 +0700

    add loading page

---
+++ src/components/loading/Loading.js
@@ -0,0 +1,14 @@
+import "./loading.css";
+export default function Loading() {
+	return (
+		<div className="container">
+			<div className="loader-container">
+				<div className="spinner"></div>
+			</div>
+
+			<div className="main-content">
+				<h1>Loading...</h1>
+			</div>
+		</div>
+	);
+}
