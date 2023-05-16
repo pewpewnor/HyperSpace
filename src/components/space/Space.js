60dd81bed0c8f04d95eaf66463645a12ea4f5291
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-05-16 18:08:38 +0700

    add navigate on click feature

--- src/components/space/Space.js
+++ src/components/space/Space.js
@@ -1,9 +1,18 @@
+import { useNavigate } from "react-router-dom";
 import shortenNumber from "utils/number";
 import "./space.css";
 
 export default function Space(props) {
+	const navigate = useNavigate();
+
 	return (
-		<div className="space__info__container">
+		<div
+			className="space__info__container"
+			onClick={() => {
+				navigate("/space/" + props.name);
+				window.location.reload();
+			}}
+		>
 			<div className="space__profile-picture">
 				{props.picture && (
 					<img
