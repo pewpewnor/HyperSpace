9d009c306ab55aa2971a3ce860ae6ef763055e56
pewpewnor <newpatchnotworking2@tutanota.com>
2023-05-17 17:01:22 +0700

    Add comment css by steven

--- src/components/comment/Comment.js
+++ src/components/comment/Comment.js
@@ -29,26 +29,33 @@ function Comment(props) {
 					<div className="comment__info__date">Posted {moment}</div>
 				</div>
 			</div>
-			<div className="comment__comment">
-				<CommentText text={props.text} />
-			</div>
-			<div className="comment__comment__button">
-				<button onClick={handleReply}>reply</button>
-			</div>
-			{reply && (
-				<div className="comment__user__comment__container">
-					<input
-						className="comment__user__comment__text"
-						placeholder="reply"
-						type={"text"}
-					/>
-					<button className="comment__user__submit__comment">
-						submit
-					</button>
+
+			<div className="comment__down__container">
+				<div className="comment__left__container"></div>
+
+				<div className="comment__right__container">
+					<div className="comment__comment">
+						<CommentText text={props.text} />
+					</div>
+					<div className="comment__comment__button">
+						<button onClick={handleReply}>reply</button>
+					</div>
+					{reply && (
+						<div className="comment__user__comment__container">
+							<input
+								className="comment__user__comment__text"
+								placeholder="reply"
+								type={"text"}
+							/>
+							<button className="comment__user__submit__comment">
+								submit
+							</button>
+						</div>
+					)}
+					<div className="comment__childcomment__list__container">
+						{props.childComments}
+					</div>
 				</div>
-			)}
-			<div className="comment__childcomment__list__container">
-				{props.childComments}
 			</div>
 		</div>
 	);
