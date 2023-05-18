a8980a316933353d66a548f53ebd6e42a353e7bf
pewpewnor <newpatchnotworking2@tutanota.com>
2023-05-18 17:02:32 +0700

    Fix login setuser error

--- src/App.js
+++ src/App.js
@@ -13,22 +13,22 @@ import SpacePage from "./pages/spacepage/SpacePage";
 import "./style.css";
 
 export default function App() {
-	// const [user, setUser] = useState(null);
-	const [user, setUser] = useState({
-		_id: "645bcbd39365d8736c77bf5a",
-		username: "ketsu",
-		password: "1234",
-		key: "c381bf796601ad28b2614d60b06471c81fbf7e9253602be498076856ef5a625b62c3afa186bfc1e0fc638ecaff60b1db",
-		joinedSpaces: [
-			"645bd00f2b7d749510bc10cb",
-			"645bd00f2b7d749510bc10cc",
-			"645bd00f2b7d749510bc10cd",
-			"645bd00f2b7d749510bc10ce",
-		],
-		profilePicture: "/user/profile-pic-1.jpg",
-		subscription: "Captain",
-		joinedDate: Date.now(),
-	});
+	const [user, setUser] = useState(null);
+	// const [user, setUser] = useState({
+	// 	_id: "645bcbd39365d8736c77bf5a",
+	// 	username: "ketsu",
+	// 	password: "1234",
+	// 	key: "c381bf796601ad28b2614d60b06471c81fbf7e9253602be498076856ef5a625b62c3afa186bfc1e0fc638ecaff60b1db",
+	// 	joinedSpaces: [
+	// 		"645bd00f2b7d749510bc10cb",
+	// 		"645bd00f2b7d749510bc10cc",
+	// 		"645bd00f2b7d749510bc10cd",
+	// 		"645bd00f2b7d749510bc10ce",
+	// 	],
+	// 	profilePicture: "/user/profile-pic-1.jpg",
+	// 	subscription: "Captain",
+	// 	joinedDate: Date.now(),
+	// });
 
 	return (
 		<UserContext.Provider value={[user, setUser]}>
